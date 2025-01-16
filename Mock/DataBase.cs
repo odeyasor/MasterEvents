dde815eb38b3a4edfef8e7ca6973449861d0c3fe
EfratSamet <148907559+EfratSamet@users.noreply.github.com>
2025-01-16 22:05:43 +0200

    Add files via upload

---
+++ Mock/DataBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Entity;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Mock
+{
+    public class DataBase:DbContext, IContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<GuestInEvent> GuestInEvents { get; set; }
+        public DbSet<Organizer> Organizers { get; set; }
+        public DbSet<PhotosFromEvent> PhotosFromEvents { get; set; }
+        public DbSet<Seating> Seatings { get; set; }
+        public DbSet<SubGuest> SubGuests { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=sql;database=MasterEvents;trusted_connection=true");
+        }
+
+        public void save()
+        {
+            SaveChanges();
+        }
+    }
+}
